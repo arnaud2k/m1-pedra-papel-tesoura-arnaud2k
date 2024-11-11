57020e05222df83650fd5df64c19153fa5fdbe81
Vinicius Arnaud <vini.arnaud14@gmail.com>
2024-11-11 20:53:14 -0300

    Resolução da Atividade Pedra Papel Tesoura

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 /* Desenvolva seu código abaixo */
+
+function playRockPaperScissor(player1, player2) {
+  const jogadasValidas = ["Pedra", "Papel", "Tesoura"];
+
+  //Caso de Empate
+  if (player1 === player2) {
+    return "Empate!";
+  }
+  //Determina o Vencedor
+  else if (
+    (player1 === "Pedra" && player2 === "Tesoura") ||
+    (player1 === "Papel" && player2 === "Pedra") ||
+    (player1 === "Tesoura" && player2 === "Papel")
+  ) {
+    return "Jogador 1 venceu!";
+  }
+
+  return "Jogador 2 venceu!";
+}
+const resultado = playRockPaperScissor("Pedra", "Tesoura");
+console.log(resultado); // Saída: "Jogador 1 venceu!"
